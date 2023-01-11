e7ec899cbb6ed725384e2be720b93d6277337b87
EpicBunty <memanu.bunty@gmail.com>
2023-01-11 15:53:37 +0530

    Lobby complete

--- Jumper/Assets/Scripts/SoundManager.cs
+++ Jumper/Assets/Scripts/SoundManager.cs
@@ -1,18 +1,104 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static SoundManager instance;
+    public static SoundManager Instance { get { return instance; } }
+
+    [SerializeField] private AudioSource soundMusic;
+
+    //[SerializeField] private AudioSource soundEffect;
+    public AudioSource soundEffect;
+    [SerializeField] private SoundType[] sounds;
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        PlayBgMusic(Sounds.BgMusic);
     }
+
+    public void PlayBgMusic(Sounds sound)
+    {
+        AudioClip clip = getSoundClip(sound);
+        if (clip != null)
+        {
+            soundMusic.clip = clip;
+            soundMusic.Play();
+        }
+        else
+        {
+            Debug.LogError("Clip not found for soundtype: " + sound);
+        }
+    }
+
+    public void Play(Sounds sound)
+    {
+        AudioClip clip = getSoundClip(sound);
+        if (clip != null)
+        {
+            soundEffect.clip = clip;
+            soundEffect.PlayOneShot(clip);
+        }
+        else
+        {
+            Debug.LogError("Clip not found for soundtype: " + sound);
+        }
+    }
+
+    public void Play(Sounds sound, float volume)
+    {
+        AudioClip clip = getSoundClip(sound);
+        if (clip != null)
+        {
+            soundEffect.clip = clip;
+            soundEffect.PlayOneShot(clip, volume);
+        }
+        else
+        {
+            Debug.LogError("Clip not found for soundtype: " + sound);
+        }
+    }
+
+    private AudioClip getSoundClip(Sounds sound)
+    {
+        SoundType sounditem = Array.Find(sounds, item => item.soundType == sound);
+        if (sounditem != null) return sounditem.soundClip;
+        return null;
+    }
+}
+
+[Serializable]
+public class SoundType
+{
+    public Sounds soundType;
+    public AudioClip soundClip;
+}
+
+public enum Sounds
+{
+    ButtonClick,
+    BgMusic,
+    PlayerMove,
+    PlayerDeath,
+    PlayerTakeDamage,
+    Collectible,
+    Teleporter,
+    ButtonBack,
+    JumpUp,
+    JumpLand,
+    NewLevel,
+    
+    
 }
